cb935628d259686df9d1b958e0dc1d13cdfb8551
nhatche42 <nhatche42@gmail.com>
2023-01-10 11:42:09 +0700

    add some function

--- campaignapp/urls.py
+++ campaignapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	path('user/<str:id>', views.get_user, name='get_user'),
 	path('csrftoken', views.get_token, name='get_token'),
 	path('addcampaign', views.add_campaign, name='add_campaign'),
+	path('allcampaign', views.get_all_campaign, name='get_all_campaign'),
+	path('campaignbyuserid/<str:user_id>', views.get_campaign_by_user_id, name='get_campaign_by_user_id'),
 ]
--- campaignapp/views.py
+++ campaignapp/views.py
@@ -11,13 +11,6 @@ from marshmallow import Schema, fields, ValidationError
 from pprint import pprint
 import datetime
 
-def say_hello(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
-
-def get_user(request, id):
-  user = User.objects.get(id=id)
-  return HttpResponse(f"user email is {user.email}")
-
 class CampaignSchema(Schema):
   name = fields.String()
   start_time = fields.DateTime()
@@ -31,6 +24,70 @@ class CampaignSchema(Schema):
   used_amount = fields.Integer()
   usage_rate = fields.Integer()
   user_id = fields.String()
+
+def say_hello(request):
+  return HttpResponse("Hello, world. You're at the polls index.")
+
+def get_user(request, id):
+  try:
+      user = User.objects.get(id=id)
+  except User.DoesNotExist:
+      return JsonResponse({"result": "user does not exist"},status=404)
+  user = User.objects.get(id=id)
+  return HttpResponse(f"user email is {user.email}")
+
+def get_all_campaign(request):
+  try:
+      campaign_dict = {}
+      campaigns = Campaign.objects.all()
+      for campaign_index in range (len(campaigns)):
+        new_item = {
+          "name": campaigns[campaign_index].name,
+          "start_time": campaigns[campaign_index].start_time,
+          "end_time": campaigns[campaign_index].end_time,
+          "budget": campaigns[campaign_index].budget,
+          "bid_amount": campaigns[campaign_index].bid_amount,
+          "title": campaigns[campaign_index].title,
+          "description": campaigns[campaign_index].description,
+          "banner": campaigns[campaign_index].banner,
+          "final_url": campaigns[campaign_index].final_url,
+          "used_amount": campaigns[campaign_index].used_amount,
+          "usage_rate": campaigns[campaign_index].usage_rate,
+          "user_id": campaigns[campaign_index].user_id_id
+        }
+        # print("new_item",new_item)
+        campaign_dict[campaign_index] = new_item
+      # print(campaign_dict)  
+      return JsonResponse({"result":campaign_dict},status=200)
+      
+  except Campaign.DoesNotExist:
+      return JsonResponse({"result": "campaign does not exist"},status=404)
+def get_campaign_by_user_id(request, user_id):
+  try:
+      campaign_dict = {}
+      campaigns = Campaign.objects.filter(user_id_id = user_id)
+      for campaign_index in range (len(campaigns)):
+        new_item = {
+          "name": campaigns[campaign_index].name,
+          "start_time": campaigns[campaign_index].start_time,
+          "end_time": campaigns[campaign_index].end_time,
+          "budget": campaigns[campaign_index].budget,
+          "bid_amount": campaigns[campaign_index].bid_amount,
+          "title": campaigns[campaign_index].title,
+          "description": campaigns[campaign_index].description,
+          "banner": campaigns[campaign_index].banner,
+          "final_url": campaigns[campaign_index].final_url,
+          "used_amount": campaigns[campaign_index].used_amount,
+          "usage_rate": campaigns[campaign_index].usage_rate,
+          "user_id": campaigns[campaign_index].user_id_id
+        }
+        # print("new_item",new_item)
+        campaign_dict[campaign_index] = new_item
+      # print(campaign_dict)  
+      return JsonResponse({"result":campaign_dict},status=200)
+      
+  except Campaign.DoesNotExist:
+      return JsonResponse({"result": "campaign does not exist"},status=404)
 
 @csrf_exempt
 def add_campaign(request):
@@ -46,16 +103,17 @@ def add_campaign(request):
     if fields != list_keys:
       return JsonResponse({"status": "error", "message": "missing fields"}, status=400)
 
-    user = User.objects.get(id=data["user_id"])
-
-    camp = Campaign(name = data["name"], start_time = data["start_time"], end_time = data["end_time"], budget = data["budget"], bid_amount = data["bid_amount"], title = data["title"], description = data["description"], banner= data["banner"], final_url= data["final_url"], used_amount = data["used_amount"], usage_rate = data["usage_rate"], user_id = user)
-    camp.save()
-    
     try:
       CampaignSchema().load(data)
     except ValidationError as err:
       pprint(err.messages)
       return JsonResponse({"status": "error validation", "message": err.messages}, status=400)
 
+    try:
+      user = User.objects.get(id=data["user_id"])
+      camp = Campaign(name = data["name"], start_time = data["start_time"], end_time = data["end_time"], budget = data["budget"], bid_amount = data["bid_amount"], title = data["title"], description = data["description"], banner= data["banner"], final_url= data["final_url"], used_amount = data["used_amount"], usage_rate = data["usage_rate"], user_id = user)
+      camp.save()
+    except User.DoesNotExist:
+      return JsonResponse({"status": "error", "message": "user does not exist"}, status= 400)
 
     return JsonResponse({"create status" : "success"})
